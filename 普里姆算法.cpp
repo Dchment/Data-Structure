fa7ebfdecbb5788c5639f303c80feb786868d0b7
Dchment <37564522+Dchment@users.noreply.github.com>
2020-04-21 10:28:25 +0800

    Update 普里姆算法.cpp

--- 普里姆算法.cpp
+++ 普里姆算法.cpp
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #define NUM 10
 #define INF 65535 
-typedef struct{//�ڽӾ���������ͼ 
+typedef struct{//邻接矩阵型无向图 
 	int node[NUM];
 	int nodevisited[NUM];
 	int matrix[NUM][NUM];
@@ -39,38 +39,37 @@ int getarc(graph &g,int a,int b){
 
 void minispantree_prim(graph g){
 	int min,i,j,k; 
-	int adjnode[NUM];//�洢���ж�������С����������˭���ӣ�һ��ÿһ��Ԫ�ص�ֵ�������Ԫ���±����ӵ����������С�������еĽ�㣨����1�� 
-	int lowweight[NUM];//�洢����������ӵ���̱ߵ�Ȩֵ ������2�� 
-	//���±�Ϊ0�ĵ������С����������Ϊ0Ϊ��ʼ���
+	int adjnode[NUM];//存储所有顶点在最小生成树中与谁连接，一般每一个元素的值代表与此元素下标连接的最近且在最小生成树中的结点（核心1） 
+	int lowweight[NUM];//存储与各顶点连接的最短边的权值 （核心2） 
+	//将下标为0的点加入最小生成树，因为0为起始结点
 	lowweight[0]=0; 
 	adjnode[0]=0;
-	//��ʼ�������±�Ϊ0�ĵ㿪ʼִ���㷨�� 
+	//初始化（从下标为0的点开始执行算法） 
 	for(i=0;i<g.nodenum;i++){
-		lowweight[i]=g.matrix[0][i];//���������±�Ϊ0�ĵ������ıߵ�Ȩֵ��û��������ֵΪINF����Ϊ������������̱ߵĳ�ֵ 
-		adjnode[i]=0;//�������ȫ��ʼ��Ϊ�±�Ϊ0�ĵ� 
+		lowweight[i]=g.matrix[0][i];//各顶点与下标为0的点相连的边的权值（没有相连则值为INF）设为各顶点相连最短边的初值 
+		adjnode[i]=0;//最近顶点全初始化为下标为0的点 
 	}
-	
 	for(i=0;i<g.nodenum;i++){
 		min=INF;
 		j=1;
 		k=0;
-		//����ȫ������ 
+		//遍历全部顶点 
 		while(j<g.nodenum){
-			if(lowweight[j]!=0/*���ǽ��������δ������С������*/&&lowweight[j]<min){
-				min=lowweight[j];//min�洢���·����Ȩֵ 
-				k=j;//k�洢Ȩֵ��С�����ӵ���һ�� 
+			if(lowweight[j]!=0/*不是结点自身且未加入最小生成树*/&&lowweight[j]<min){
+				min=lowweight[j];//min存储最短路径的权值 
+				k=j;//k存储权值最小边连接的另一端 
 			}
 			j++;
 		}
 		
-		printf("����%d �����˶���%d\n",adjnode[k],k);
-		lowweight[k]=0;//����ǰ����Ȩֵ��Ϊ0�������Ѽ�����С����������������� 
-		//���ڽӾ����k�п�ʼ������������һ��Ӧ�����ӵıߣ������һ��Ϊ�Ѿ�������С�������Ķ���k�� 
+		printf("顶点%d 连接了顶点%d，长度为%d\n",adjnode[k],k,lowweight[k]);
+		lowweight[k]=0;//将当前顶点k的权值设为0，表明已加入最小生成树，完成了任务 
+		//对邻接矩阵的k行开始遍历，即寻找下一个应该连接的边，其起点一端为已经加入最小生成树的当前顶点k！ 
 		for(j=1;j<g.nodenum;j++){
 			if(lowweight[j]!=0&&g.matrix[k][j]<lowweight[j]){
-				lowweight[j]=g.matrix[k][j];//��j��������̱�Ϊk-j�� 
-				adjnode[j]=k;//k-j�߼�����С������ 
-			}//ͬ�ղű���һ�����ҳ����·�� 
+				lowweight[j]=g.matrix[k][j];//与j相连的最短边为k-j边 
+				adjnode[j]=k;//k-j边加入最小生成树 
+			}//同刚才遍历一样，找出最短路径 
 		} 
 	}
 } 
